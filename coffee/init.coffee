6963dbcbbc6977c4213461aa77ee96bc1c6d7fa0
pandeiro245 <nishiko@mindia.jp>
2013-05-04 20:22:22 +0900

    refs #1

--- coffee/init.coffee
+++ coffee/init.coffee
@@ -1,75 +1,138 @@
 working_log = null
+domain = null
+token = null
+last_fetch = null
+
+getLastFetch = () ->
+  info = db.one("infos", {key: "last_fetch"})
+  if info then info.val else 0
+
+getDomain = () ->
+  info = db.one("infos", {key: "domain"})
+  if !info or  info.val.length < 10
+    val = prompt('please input the domain', 'http://crowdsourcing.dev')
+    info = setInfo("domain", val)
+  return info.val
+
+getToken = () ->
+  info = db.one("infos", {key: "token"})
+  if !info or  info.val.length < 10
+    val = prompt('please input your API token', '')
+    info = setInfo("token",val)
+  return info.val
 
 init = () ->
+  #setInfo("last_fetch", 0)
+  domain = getDomain() unless domain
+  token = getToken() unless token
+  last_fetch = getLastFetch() unless last_fetch
   renderProjects()
   renderIssues()
   fetch()
+  loopFetch()
+  loopRenderWorkLogs()
 
 fetch = () ->
-  url = "http://crowdsourcing.dev/api/v1/projects.json?device_token=1"
-  $.get(url, (projects) ->
-    syncProjects(projects)
-    renderProjects(projects)
-    url = "http://crowdsourcing.dev/api/v1/issues.json?device_token=1"
-    $.get(url, (issues) ->
-      syncIssues(issues)
-      renderIssues(issues)
-    )
+  projects = []
+  issues = []
+  work_logs = []
+  for p in db.find("projects")
+    project = db.one("issues", {project_id: p.id})
+    projects.push(p) if project && !project.server_id
+  for i in db.find("issues")
+    issues.push(i) if !i.server_id
+  for w in db.find("work_logs")
+    work_logs.push(w) if !w.server_id
+  diffs =  {
+    projects: projects,
+    issues: issues,
+    work_logs: work_logs
+  }
+  params = {
+    token: token,
+    last_fetch: last_fetch,
+    diffs: diffs
+  }
+  console.log "fetch at #{now()}"
+  url = "#{domain}/api/v1/diffs.json"
+  $.post(url, params, (data) ->
+    sync("server_ids", data.server_ids)
+    sync("projects", data.projects)
+    sync("issues", data.issues)
+    sync("work_logs", data.work_logs)
+    last_fetch = parseInt(new Date().getTime())
+    setInfo("last_fetch", last_fetch)
   )
 
-syncProjects = (projects) ->
-  for p in projects
-    cond = {server_id: p.id}
-    project = db.one("projects", cond)
-    if project
-      db.upd("projects", project)
-    else
-      cond.name = p.name
-      cond.body = p.body
-      db.ins("projects", cond)
+sync = (table_name, data) ->
+  if table_name == "server_ids"
+    for table_name, local_ids of data 
+      for local_id, server_id of local_ids
+        item = db.one(table_name, {id: local_id})
+        item.server_id = server_id
+        db.upd(table_name, item)
+  else
+    for i in data
+      if i.project_id
+        i.project_id = db.one("projects", {server_id: i.project_id}).id
+      if i.issue_id
+        issue = db.one("issues", {server_id: i.issue_id})
+        i.issue_id = issue.id
+      if i.closed_at
+        i.is_closed = true
+      item = db.one(table_name, {server_id: i.id})
+      if item
+        i.id = item.id
+        item = i
+        db.upd(table_name, item)
+      else
+        i.server_id = i.id
+        delete i.id
+        item = db.ins(table_name, i)
+    console.log "sync is done"
 
-syncIssues = (issues) ->
-  for i in issues
-    cond = {server_id: i.id}
-    issue = db.one("issues", cond)
-    cond.title = i.title
-    cond.project_id = db.one("projects", {server_id: i.project_id}).id
-    cond.body = i.body
-    if issue
-      db.upd("issues", issue)
-    else
-      db.ins("issues", cond)
-
-
-renderProjects = (projects) ->
-  projects = db.find("projects")
-  $("#projects-tab").html("")
+renderProjects = (projects=null) ->
+  projects = db.find("projects") unless projects
+  $("#projects-tab").html("<div id=\"new_project\"></div>")
+  $("#new_project").html("""
+      <input type=\"text\" class=\"input\" />
+      <input type=\"submit\" value=\"add project\" />
+  """)
   for project in projects
-    $("#projects-tab").append("""
-      <div id=\"project_#{project.id}\" class=\"project\">
-        <h1>#{project.name}</h1>
-        <input type=\"text\" class=\"input\" />
-        <input type=\"submit\" value=\"add issue\" />
-        <div class="issues"></div>
-      </div>
-    """)
-
+    renderProject(project)
   hl.enter(".input", (e, target)->
-    project_id = $(target).parent().attr("id").replace("project_","")
     title = $(target).val()
-    if title.length > 0
-      addIssue(project_id, title)
-      $("#input").val("")
-    else
-      alert "please input the title"
+    $project = $(target).parent()
+    if $project.attr("id").match("project_")
+      project_id = $project.attr("id").replace("project_","")
+      if title.length > 0
+        addIssue(project_id, title)
+        $(target).val("")
+      else
+        alert "please input the title"
+    else #new_project
+      if title.length > 0
+        addProject(title)
+        $(target).val("")
+      else
+        alert "please input the title"
   )
 
+renderProject = (project) ->
+  $("#projects-tab").append("""
+    <div id=\"project_#{project.id}\" class=\"project\">
+      <h1>#{project.name}</h1>
+      <input type=\"text\" class=\"input\" />
+      <input type=\"submit\" value=\"add issue\" />
+      <div class="issues"></div>
+    </div>
+  """)
 
-renderIssues = (issues) ->
-  issues = db.find("issues")
+renderIssues = (issues=null) ->
+  issues = db.find("issues",{is_closed: false, assignee_id: 1}) unless issues
   $(".issues").html("")
   for issue in issues
-    renderIssue(issue)
+    renderIssue(issue) if !issue.is_ddt
   renderCards()
   $(() ->
     $(".issue .title").click(() ->
@@ -85,59 +148,140 @@ renderIssues = (issues) ->
       renderCards(issue_id)
       return false
     )
+    $(".ddt").click(() ->
+      issue_id = $(this).parent().attr("id").replace("issue_", "")
+      renderDdt(issue_id)
+      return false
+    )
   )
 
 renderIssue = (issue) ->
   $project = $("#project_#{issue.project_id}")
   $project.css("display", "block")
-  title = issue.title
-  title = "<a class=\"title\" href=\"#\">#{issue.title}</a>" if issue.body.length > 0
+  title = "#{issue.title} #{issue.is_ddt}"
+  title = "<a class=\"title\" href=\"#\">#{issue.title}</a>" if issue.body && issue.body.length > 0
   $project.append("""
     <div id=\"issue_#{issue.id}\" class=\"issue\">
       #{title} 
       <a class=\"card\" href="#"></a>
+      <a class=\"ddt\" href="#">DDT</a>
       <div class=\"body\">#{issue.body}</div>
     </div>
   """)
 
+renderDdt = (issue_id) ->
+  issue = db.one("issues", {id: issue_id})
+  issue.is_ddt = true
+  db.upd("issues", issue)
+  $("#issue_#{issue.id}").css("display", "none")
+
 renderCards = (issue_id = null) ->
-  if !issue_id 
-    $(".card").html("start")
+  if working_log
+    stopWorkLog() if issue_id
+    if  parseInt(issue_id) != parseInt(working_log.issue_id)
+      startWorkLog(issue_id)
+    else
+      working_log = null
   else
-    if working_log
-      stopWorkLog()
-      if  Math.floor(issue_id) != Math.floor(working_log.issue_id)
-        startWorkLog(issue_id)
-      else
-        working_log = null
+    if !issue_id 
+      $(".card").html("start")
     else
       startWorkLog(issue_id)
   renderWorkLogs()
 
 stopWorkLog = () ->
-  working_log.end_at = new Date().getTime()
+  console.log work_log
+  working_log.end_at = now()
   db.upd("work_logs", working_log)
   $("#issue_#{working_log.issue_id} .card").html("start")
 
-startWorkLog = (issue_id) ->
-  working_log = db.ins("work_logs", {issue_id: issue_id})
-  working_log.started_at = working_log.ins_at
-  db.upd("work_logs", working_log)
-  $("#issue_#{issue_id} .card").html("stop")
+startWorkLog = (issue_id = null) ->
+  if issue_id
+    working_log = db.ins("work_logs", {issue_id: issue_id})
+    working_log.started_at = working_log.ins_at
+    db.upd("work_logs", working_log)
+    $("#issue_#{issue_id} .card").html("stop")
 
 addIssue = (project_id, title) ->
-  issue = db.ins("issues", {title: title, project_id: project_id, body:""})
+  issue = db.ins("issues", {title: title, project_id: project_id, body:"", assignee_id:1, user_id:1})
   renderIssue(issue)
+
+addProject = (name) ->
+  project = db.ins("projects", {name: name})
+  #renderProject(project)
+  renderProjects()
+  $("#project_#{project.id}").css("display", "block")
 
 renderWorkLogs = () ->
   $("#work_logs").html("")
+  title = "crowdsourciing"
   for work_log in db.find("work_logs")
-    issue = db.one("issues", work_log.issue_id)
+    if !work_log.issue_id
+      url = "#{domain}/api/v1/work_logs/#{work_log.server_id}.json?token=#{token}"
+      $.get(url, (data) ->
+        issue = db.one("issues", {server_id: data.id})
+        work_log.issue_id = issue.id
+        db.upd("work_logs", work_log)
+      )
+    console.log work_log
+    issue = db.one("issues", {id: work_log.issue_id})
+    stop = ""
+    stop = "<a href=\"#\" class=\"cardw\">STOP</a>" unless work_log.end_at
     $("#work_logs").prepend("""
-      <div>#{issue.title} #{parseInt((work_log.end_at - work_log.started_at)/1000)}</div>
+      <div>#{issue.title} #{dispTime(work_log)}</div>#{stop}
     """)
+    $(".cardw").click(() ->
+      issue_id = working_log.issue_id
+      renderCards(issue_id)
+      return false
+    )
+    if !work_log.end_at
+      title = dispTime(work_log)
+      working_log = work_log
+  title
+
+loopRenderWorkLogs = () ->
+  window.title = renderWorkLogs()
+  setTimeout(()->
+    loopRenderWorkLogs()
+  ,1000)
+
+loopFetch = () ->
+  if last_fetch > 0
+    fetch()
+    setTimeout(()->
+      loopFetch()
+    ,1000*10)
+
+dispTime = (work_log) ->
+  msec = 0
+  if work_log.end_at
+    msec = work_log.end_at - work_log.started_at 
+  else
+    msec = parseInt(new Date().getTime()) - work_log.started_at
+  sec = parseInt((msec)/1000)
+  if sec > 60
+    min = parseInt(sec/60)
+    res = "#{zero(min)}:#{zero(sec - min*60)}"
+  else
+    res = "#{sec}秒"
+  res
+
+setInfo = (key, val) ->
+  info = db.one("infos", {key: key})
+  if info
+    info.val = val
+    info = db.upd("infos", info)
+  else
+    info = db.ins("infos", {key: key, val: val})
+  info
 
 schema = {
+  users: {
+    server_id:0,
+    name: ""
+    #$uniques: "server_id"
+  }
   projects: {
     name: ""
     body: "",
@@ -149,12 +293,28 @@ schema = {
     body: "",
     project_id: 0,
     server_id: 0,
+    is_ddt: "off",
+    is_closed: "off",
+    user_id: 0,
+    assignee_id: 0,
     #$uniques: "server_id"
   },
   work_logs: {
     issue_id: 0
     started_at: 0,
     end_at: 0,
+    server_id: 0,
+    #$uniques: "server_id"
+  },
+  work_comments: {
+    issue_id: 0,
+    user_id: 0,
+    body: "",
+  }
+  infos: {
+    key: "",
+    val: "",
+    $uniques: "key"
   }
 }
 
@@ -164,6 +324,12 @@ db = JSRel.use("crowdsourcing", {
 })
 
 hl = window.helpers
+
+zero = (int) ->
+  if int < 10 then "0#{int}" else int
+
+now = () ->
+  new Date().getTime()
 
 $(() ->
   init()
--- public/js/coffee.js
+++ public/js/coffee.js
@@ -41,107 +41,242 @@
 }).call(this);
 
 (function() {
-  var addIssue, db, fetch, hl, init, renderCards, renderIssue, renderIssues, renderProjects, renderWorkLogs, schema, startWorkLog, stopWorkLog, syncIssues, syncProjects, working_log;
+  var addIssue, addProject, db, dispTime, domain, fetch, getDomain, getLastFetch, getToken, hl, init, last_fetch, loopFetch, loopRenderWorkLogs, now, renderCards, renderDdt, renderIssue, renderIssues, renderProject, renderProjects, renderWorkLogs, schema, setInfo, startWorkLog, stopWorkLog, sync, token, working_log, zero;
 
   working_log = null;
 
+  domain = null;
+
+  token = null;
+
+  last_fetch = null;
+
+  getLastFetch = function() {
+    var info;
+
+    info = db.one("infos", {
+      key: "last_fetch"
+    });
+    if (info) {
+      return info.val;
+    } else {
+      return 0;
+    }
+  };
+
+  getDomain = function() {
+    var info, val;
+
+    info = db.one("infos", {
+      key: "domain"
+    });
+    if (!info || info.val.length < 10) {
+      val = prompt('please input the domain', 'http://crowdsourcing.dev');
+      info = setInfo("domain", val);
+    }
+    return info.val;
+  };
+
+  getToken = function() {
+    var info, val;
+
+    info = db.one("infos", {
+      key: "token"
+    });
+    if (!info || info.val.length < 10) {
+      val = prompt('please input your API token', '');
+      info = setInfo("token", val);
+    }
+    return info.val;
+  };
+
   init = function() {
+    if (!domain) {
+      domain = getDomain();
+    }
+    if (!token) {
+      token = getToken();
+    }
+    if (!last_fetch) {
+      last_fetch = getLastFetch();
+    }
     renderProjects();
     renderIssues();
-    return fetch();
+    fetch();
+    loopFetch();
+    return loopRenderWorkLogs();
   };
 
   fetch = function() {
-    var url;
+    var diffs, i, issues, p, params, project, projects, url, w, work_logs, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2;
 
-    url = "http://crowdsourcing.dev/api/v1/projects.json?device_token=1";
-    return $.get(url, function(projects) {
-      syncProjects(projects);
-      renderProjects(projects);
-      url = "http://crowdsourcing.dev/api/v1/issues.json?device_token=1";
-      return $.get(url, function(issues) {
-        syncIssues(issues);
-        return renderIssues(issues);
+    projects = [];
+    issues = [];
+    work_logs = [];
+    _ref = db.find("projects");
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      p = _ref[_i];
+      project = db.one("issues", {
+        project_id: p.id
       });
+      if (project && !project.server_id) {
+        projects.push(p);
+      }
+    }
+    _ref1 = db.find("issues");
+    for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+      i = _ref1[_j];
+      if (!i.server_id) {
+        issues.push(i);
+      }
+    }
+    _ref2 = db.find("work_logs");
+    for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+      w = _ref2[_k];
+      if (!w.server_id) {
+        work_logs.push(w);
+      }
+    }
+    diffs = {
+      projects: projects,
+      issues: issues,
+      work_logs: work_logs
+    };
+    params = {
+      token: token,
+      last_fetch: last_fetch,
+      diffs: diffs
+    };
+    console.log("fetch at " + (now()));
+    url = "" + domain + "/api/v1/diffs.json";
+    return $.post(url, params, function(data) {
+      sync("server_ids", data.server_ids);
+      sync("projects", data.projects);
+      sync("issues", data.issues);
+      sync("work_logs", data.work_logs);
+      last_fetch = parseInt(new Date().getTime());
+      return setInfo("last_fetch", last_fetch);
     });
   };
 
-  syncProjects = function(projects) {
-    var cond, p, project, _i, _len, _results;
+  sync = function(table_name, data) {
+    var i, issue, item, local_id, local_ids, server_id, _i, _len, _results;
 
-    _results = [];
-    for (_i = 0, _len = projects.length; _i < _len; _i++) {
-      p = projects[_i];
-      cond = {
-        server_id: p.id
-      };
-      project = db.one("projects", cond);
-      if (project) {
-        _results.push(db.upd("projects", project));
-      } else {
-        cond.name = p.name;
-        cond.body = p.body;
-        _results.push(db.ins("projects", cond));
+    if (table_name === "server_ids") {
+      _results = [];
+      for (table_name in data) {
+        local_ids = data[table_name];
+        _results.push((function() {
+          var _results1;
+
+          _results1 = [];
+          for (local_id in local_ids) {
+            server_id = local_ids[local_id];
+            item = db.one(table_name, {
+              id: local_id
+            });
+            item.server_id = server_id;
+            _results1.push(db.upd(table_name, item));
+          }
+          return _results1;
+        })());
       }
-    }
-    return _results;
-  };
-
-  syncIssues = function(issues) {
-    var cond, i, issue, _i, _len, _results;
-
-    _results = [];
-    for (_i = 0, _len = issues.length; _i < _len; _i++) {
-      i = issues[_i];
-      cond = {
-        server_id: i.id
-      };
-      issue = db.one("issues", cond);
-      cond.title = i.title;
-      cond.project_id = db.one("projects", {
-        server_id: i.project_id
-      }).id;
-      cond.body = i.body;
-      if (issue) {
-        _results.push(db.upd("issues", issue));
-      } else {
-        _results.push(db.ins("issues", cond));
+      return _results;
+    } else {
+      for (_i = 0, _len = data.length; _i < _len; _i++) {
+        i = data[_i];
+        if (i.project_id) {
+          i.project_id = db.one("projects", {
+            server_id: i.project_id
+          }).id;
+        }
+        if (i.issue_id) {
+          issue = db.one("issues", {
+            server_id: i.issue_id
+          });
+          i.issue_id = issue.id;
+        }
+        if (i.closed_at) {
+          i.is_closed = true;
+        }
+        item = db.one(table_name, {
+          server_id: i.id
+        });
+        if (item) {
+          i.id = item.id;
+          item = i;
+          db.upd(table_name, item);
+        } else {
+          i.server_id = i.id;
+          delete i.id;
+          item = db.ins(table_name, i);
+        }
       }
+      return console.log("sync is done");
     }
-    return _results;
   };
 
   renderProjects = function(projects) {
     var project, _i, _len;
 
-    projects = db.find("projects");
-    $("#projects-tab").html("");
+    if (projects == null) {
+      projects = null;
+    }
+    if (!projects) {
+      projects = db.find("projects");
+    }
+    $("#projects-tab").html("<div id=\"new_project\"></div>");
+    $("#new_project").html("<input type=\"text\" class=\"input\" />\n<input type=\"submit\" value=\"add project\" />");
     for (_i = 0, _len = projects.length; _i < _len; _i++) {
       project = projects[_i];
-      $("#projects-tab").append("<div id=\"project_" + project.id + "\" class=\"project\">\n  <h1>" + project.name + "</h1>\n  <input type=\"text\" class=\"input\" />\n  <input type=\"submit\" value=\"add issue\" />\n  <div class=\"issues\"></div>\n</div>");
+      renderProject(project);
     }
     return hl.enter(".input", function(e, target) {
-      var project_id, title;
+      var $project, project_id, title;
 
-      project_id = $(target).parent().attr("id").replace("project_", "");
       title = $(target).val();
-      if (title.length > 0) {
-        addIssue(project_id, title);
-        return $("#input").val("");
+      $project = $(target).parent();
+      if ($project.attr("id").match("project_")) {
+        project_id = $project.attr("id").replace("project_", "");
+        if (title.length > 0) {
+          addIssue(project_id, title);
+          return $(target).val("");
+        } else {
+          return alert("please input the title");
+        }
       } else {
-        return alert("please input the title");
+        if (title.length > 0) {
+          addProject(title);
+          return $(target).val("");
+        } else {
+          return alert("please input the title");
+        }
       }
     });
+  };
+
+  renderProject = function(project) {
+    return $("#projects-tab").append("<div id=\"project_" + project.id + "\" class=\"project\">\n  <h1>" + project.name + "</h1>\n  <input type=\"text\" class=\"input\" />\n  <input type=\"submit\" value=\"add issue\" />\n  <div class=\"issues\"></div>\n</div>");
   };
 
   renderIssues = function(issues) {
     var issue, _i, _len;
 
-    issues = db.find("issues");
+    if (issues == null) {
+      issues = null;
+    }
+    if (!issues) {
+      issues = db.find("issues", {
+        is_closed: false,
+        assignee_id: 1
+      });
+    }
     $(".issues").html("");
     for (_i = 0, _len = issues.length; _i < _len; _i++) {
       issue = issues[_i];
-      renderIssue(issue);
+      if (!issue.is_ddt) {
+        renderIssue(issue);
+      }
     }
     renderCards();
     return $(function() {
@@ -156,11 +291,18 @@
         }
         return false;
       });
-      return $(".card").click(function() {
+      $(".card").click(function() {
         var issue_id;
 
         issue_id = $(this).parent().attr("id").replace("issue_", "");
         renderCards(issue_id);
+        return false;
+      });
+      return $(".ddt").click(function() {
+        var issue_id;
+
+        issue_id = $(this).parent().attr("id").replace("issue_", "");
+        renderDdt(issue_id);
         return false;
       });
     });
@@ -171,27 +313,40 @@
 
     $project = $("#project_" + issue.project_id);
     $project.css("display", "block");
-    title = issue.title;
-    if (issue.body.length > 0) {
+    title = "" + issue.title + " " + issue.is_ddt;
+    if (issue.body && issue.body.length > 0) {
       title = "<a class=\"title\" href=\"#\">" + issue.title + "</a>";
     }
-    return $project.append("<div id=\"issue_" + issue.id + "\" class=\"issue\">\n  " + title + " \n  <a class=\"card\" href=\"#\"></a>\n  <div class=\"body\">" + issue.body + "</div>\n</div>");
+    return $project.append("<div id=\"issue_" + issue.id + "\" class=\"issue\">\n  " + title + " \n  <a class=\"card\" href=\"#\"></a>\n  <a class=\"ddt\" href=\"#\">DDT</a>\n  <div class=\"body\">" + issue.body + "</div>\n</div>");
+  };
+
+  renderDdt = function(issue_id) {
+    var issue;
+
+    issue = db.one("issues", {
+      id: issue_id
+    });
+    issue.is_ddt = true;
+    db.upd("issues", issue);
+    return $("#issue_" + issue.id).css("display", "none");
   };
 
   renderCards = function(issue_id) {
     if (issue_id == null) {
       issue_id = null;
     }
-    if (!issue_id) {
-      $(".card").html("start");
-    } else {
-      if (working_log) {
+    if (working_log) {
+      if (issue_id) {
         stopWorkLog();
-        if (Math.floor(issue_id) !== Math.floor(working_log.issue_id)) {
-          startWorkLog(issue_id);
-        } else {
-          working_log = null;
-        }
+      }
+      if (parseInt(issue_id) !== parseInt(working_log.issue_id)) {
+        startWorkLog(issue_id);
+      } else {
+        working_log = null;
+      }
+    } else {
+      if (!issue_id) {
+        $(".card").html("start");
       } else {
         startWorkLog(issue_id);
       }
@@ -200,18 +355,24 @@
   };
 
   stopWorkLog = function() {
-    working_log.end_at = new Date().getTime();
+    console.log(work_log);
+    working_log.end_at = now();
     db.upd("work_logs", working_log);
     return $("#issue_" + working_log.issue_id + " .card").html("start");
   };
 
   startWorkLog = function(issue_id) {
-    working_log = db.ins("work_logs", {
-      issue_id: issue_id
-    });
-    working_log.started_at = working_log.ins_at;
-    db.upd("work_logs", working_log);
-    return $("#issue_" + issue_id + " .card").html("stop");
+    if (issue_id == null) {
+      issue_id = null;
+    }
+    if (issue_id) {
+      working_log = db.ins("work_logs", {
+        issue_id: issue_id
+      });
+      working_log.started_at = working_log.ins_at;
+      db.upd("work_logs", working_log);
+      return $("#issue_" + issue_id + " .card").html("stop");
+    }
   };
 
   addIssue = function(project_id, title) {
@@ -220,26 +381,125 @@
     issue = db.ins("issues", {
       title: title,
       project_id: project_id,
-      body: ""
+      body: "",
+      assignee_id: 1,
+      user_id: 1
     });
     return renderIssue(issue);
   };
 
+  addProject = function(name) {
+    var project;
+
+    project = db.ins("projects", {
+      name: name
+    });
+    renderProjects();
+    return $("#project_" + project.id).css("display", "block");
+  };
+
   renderWorkLogs = function() {
-    var issue, work_log, _i, _len, _ref, _results;
+    var issue, stop, title, url, work_log, _i, _len, _ref;
 
     $("#work_logs").html("");
+    title = "crowdsourciing";
     _ref = db.find("work_logs");
-    _results = [];
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       work_log = _ref[_i];
-      issue = db.one("issues", work_log.issue_id);
-      _results.push($("#work_logs").prepend("<div>" + issue.title + " " + (parseInt((work_log.end_at - work_log.started_at) / 1000)) + "</div>"));
+      if (!work_log.issue_id) {
+        url = "" + domain + "/api/v1/work_logs/" + work_log.server_id + ".json?token=" + token;
+        $.get(url, function(data) {
+          var issue;
+
+          issue = db.one("issues", {
+            server_id: data.id
+          });
+          work_log.issue_id = issue.id;
+          return db.upd("work_logs", work_log);
+        });
+      }
+      console.log(work_log);
+      issue = db.one("issues", {
+        id: work_log.issue_id
+      });
+      stop = "";
+      if (!work_log.end_at) {
+        stop = "<a href=\"#\" class=\"cardw\">STOP</a>";
+      }
+      $("#work_logs").prepend("<div>" + issue.title + " " + (dispTime(work_log)) + "</div>" + stop);
+      $(".cardw").click(function() {
+        var issue_id;
+
+        issue_id = working_log.issue_id;
+        renderCards(issue_id);
+        return false;
+      });
+      if (!work_log.end_at) {
+        title = dispTime(work_log);
+        working_log = work_log;
+      }
     }
-    return _results;
+    return title;
+  };
+
+  loopRenderWorkLogs = function() {
+    window.title = renderWorkLogs();
+    return setTimeout(function() {
+      return loopRenderWorkLogs();
+    }, 1000);
+  };
+
+  loopFetch = function() {
+    if (last_fetch > 0) {
+      fetch();
+      return setTimeout(function() {
+        return loopFetch();
+      }, 1000 * 10);
+    }
+  };
+
+  dispTime = function(work_log) {
+    var min, msec, res, sec;
+
+    msec = 0;
+    if (work_log.end_at) {
+      msec = work_log.end_at - work_log.started_at;
+    } else {
+      msec = parseInt(new Date().getTime()) - work_log.started_at;
+    }
+    sec = parseInt(msec / 1000);
+    if (sec > 60) {
+      min = parseInt(sec / 60);
+      res = "" + (zero(min)) + ":" + (zero(sec - min * 60));
+    } else {
+      res = "" + sec + "秒";
+    }
+    return res;
+  };
+
+  setInfo = function(key, val) {
+    var info;
+
+    info = db.one("infos", {
+      key: key
+    });
+    if (info) {
+      info.val = val;
+      info = db.upd("infos", info);
+    } else {
+      info = db.ins("infos", {
+        key: key,
+        val: val
+      });
+    }
+    return info;
   };
 
   schema = {
+    users: {
+      server_id: 0,
+      name: ""
+    },
     projects: {
       name: "",
       body: "",
@@ -249,12 +509,27 @@
       title: "",
       body: "",
       project_id: 0,
-      server_id: 0
+      server_id: 0,
+      is_ddt: "off",
+      is_closed: "off",
+      user_id: 0,
+      assignee_id: 0
     },
     work_logs: {
       issue_id: 0,
       started_at: 0,
-      end_at: 0
+      end_at: 0,
+      server_id: 0
+    },
+    work_comments: {
+      issue_id: 0,
+      user_id: 0,
+      body: ""
+    },
+    infos: {
+      key: "",
+      val: "",
+      $uniques: "key"
     }
   };
 
@@ -264,6 +539,18 @@
   });
 
   hl = window.helpers;
+
+  zero = function(int) {
+    if (int < 10) {
+      return "0" + int;
+    } else {
+      return int;
+    }
+  };
+
+  now = function() {
+    return new Date().getTime();
+  };
 
   $(function() {
     return init();
