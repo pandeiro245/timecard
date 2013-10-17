c2d32e8cfe5bf4794f6e0040cec15db1ffd29496
Kota Miyake <miyake.kota@gmail.com>
2013-10-17 15:38:51 +0800

    コメント作成、編集、削除機能作成 #24

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ActiveRecord::Base
   belongs_to :project
   belongs_to :author, class_name: "User", foreign_key: :author_id
   belongs_to :assignee, class_name: "User", foreign_key: :assignee_id
+  has_many :comments
   has_many :work_logs
 
   validates :subject, presence: true
